a52933a47c30527ed86dc33bf4b76e6a3a375d9e
Dave Syer <dsyer@vmware.com>
2020-10-01 06:41:29 +0000

    Make it work

--- samples/db/src/main/java/app/main/SampleApplication.java
+++ samples/db/src/main/java/app/main/SampleApplication.java
@@ -1,19 +1,23 @@
 package app.main;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-import static org.springframework.web.reactive.function.server.ServerResponse.ok;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.server.RouterFunction;
-
 import app.main.foo.Foo;
 import app.main.foo.FooRepository;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
+
+import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.web.reactive.function.server.RouterFunction;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @SpringBootApplication(proxyBeanMethods = false)
 public class SampleApplication {
@@ -41,7 +45,16 @@ public class SampleApplication {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(SampleApplication.class, args);
+		new SpringApplicationBuilder(SampleApplication.class).initializers(SampleApplication::process).run(args);
+	}
+
+	private static void process(ConfigurableApplicationContext registry) {
+		if (!registry.containsBeanDefinition(AnnotationConfigUtils.AUTOWIRED_ANNOTATION_PROCESSOR_BEAN_NAME)) {
+			((GenericApplicationContext) registry).registerBean(
+					AnnotationConfigUtils.AUTOWIRED_ANNOTATION_PROCESSOR_BEAN_NAME,
+					AutowiredAnnotationBeanPostProcessor.class, () -> new AutowiredAnnotationBeanPostProcessor());
+		}
+
 	}
 
 }
